e4add58c59abfbca701b6b71659de2d62473b3cd
Martin Palkovic <martin@palkovic.org>
2025-05-31 11:07:50 -0300

    chore: minor sql file updates

--- qgis/virtual_fields/elev_descent_meters.sql
+++ qgis/virtual_fields/elev_descent_meters.sql
@@ -3,7 +3,7 @@ with_variable('elev_descent', 0,
     array_sum(
         array_foreach(
             generate_series(1, num_points($geometry)-1),
-            max(0, raster_value('raster_layer', 1, point_n($geometry, @element)) - raster_value('raster_layer', 1, point_n($geometry, @element+1)))
+            max(0, raster_value('dem_phase1_earth', 1, point_n($geometry, @element)) - raster_value('dem_phase1_earth', 1, point_n($geometry, @element+1)))
         )
     )
 )
--- qgis/virtual_fields/elev_gain_meters.sql
+++ qgis/virtual_fields/elev_gain_meters.sql
@@ -3,7 +3,7 @@ with_variable('elev_gain', 0,
     array_sum(
         array_foreach(
             generate_series(1, num_points($geometry)-1),
-            max(0, raster_value('raster_layer', 1, point_n($geometry, @element+1)) - raster_value('raster_layer', 1, point_n($geometry, @element)))
+            max(0, raster_value('dem_phase1_earth', 1, point_n($geometry, @element+1)) - raster_value('dem_phase1_earth', 1, point_n($geometry, @element)))
         )
     )
 )
